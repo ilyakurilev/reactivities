8f18e33544bed33107cc1307b624917660f83bd5
ilya <ilyakurilev@gmail.com>
2021-03-24 11:36:54 +0300

    add react routing

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,12 +5,15 @@ import './app/layout/style.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <StoreContext.Provider value={store}>
+  <StoreContext.Provider value={store}>
+    <BrowserRouter>
       <App />
-    </StoreContext.Provider>
-    ,
+    </BrowserRouter>
+  </StoreContext.Provider>
+  ,
   document.getElementById('root')
 );
 
